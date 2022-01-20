c1333f7aaf44ec2579b0ed2f3cc977a5319b6a4f
kamal673 <kamal673@yandex.ru>
2022-01-20 20:08:28 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 7.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HomeWork7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну ребра куба:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Обьем куба:" + Math.Pow(a, 3) + " куб.см");
+            Console.WriteLine("Площадь поверхности:" + Math.Pow(a, 2) * 6 + " кв.см");
+            Console.ReadLine();
+        }
+    }
+}
